ffceaf9c16fb5a403aa840e97a33ef497b44c15e
Saikumar guduru <skg.13.edu@gmail.com>
2024-03-29 14:51:49 +0530

    vercel deploy error fix

--- src/components/Admin/AdminInvoiceAccordion.js
+++ src/components/Admin/AdminInvoiceAccordion.js
@@ -3,44 +3,49 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import copy from "clipboard-copy";
-import "./AdminInvoiceManager.css";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-// Component for managing admin invoices
-const AdminInvoiceAccordion = ({ invoice, code, pdfUrl, preSignedUrl }) => {
- // State to manage the accordion open/close status
- const [isAccordionOpen, setAccordionOpen] = useState(false);
- // Hook for navigation within the app
- const navigate = useNavigate();
- // Environment variables for API endpoints and time
- const API = process.env.REACT_APP_API;
- const API2 = process.env.REACT_APP_URL;
- const hours = process.env.REACT_APP_TIME;
+const AdminInvoiceAccordion = ({ invoice, code, pdfUrl, preSignedUrl}) => {
+  const [isAccordionOpen, setAccordionOpen] = useState(false);
+  const navigate = useNavigate();
+  const API = process.env.REACT_APP_API;
+  const API2 = process.env.REACT_APP_URL;
+	const hours = process.env.REACT_APP_TIME;
 
- // Selected invoice details
- const selectedInvoiceId = invoice;
- const selectedCode = code;
- const selectedPdfUrl = pdfUrl;
- const selectedPreSignedUrll = preSignedUrl;
- const [shortenedUrl, setShortenedUrl] = useState("");
- // Check if the user is on a mobile device
- const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
- // Original PDF URL for viewing
- const pdfUrlOriginal = `${API}download/${selectedInvoiceId}`;
- const ViewURLOriginal = `https://docs.google.com/viewer?url=${encodeURIComponent(
+  const selectedInvoiceId = invoice;
+  const selectedCode = code;
+  const selectedPdfUrl = pdfUrl;
+  const selectedPreSignedUrll = preSignedUrl;
+  const [shortenedUrl, setShortenedUrl] = useState("");
+  // console.log(selectedPdfUrl);
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+  const pdfUrlOriginal = `${API}download/${selectedInvoiceId}`;
+  const ViewURLOriginal = `https://docs.google.com/viewer?url=${encodeURIComponent(
     pdfUrlOriginal
- )}&embedded=true`;
+  )}&embedded=true`;
 
- // Function to toggle the accordion open/close status
- const toggleAccordion = () => {
+  // const shareUrl = `${API}web/viewer.html?file=${encodeURIComponent(pdfUrl)}`;
+
+  const toggleAccordion = () => {
     setAccordionOpen(!isAccordionOpen);
- };
+  };
+  // const pdfUrl = `${selectedInvoiceId}`;
 
- // Function to handle viewing the invoice
- const handleView = () => {
-    // Open in a new window for mobile devices
+  // const handleOriginalInvoice = () => {
+  // 	const expirationTimestamp = Date.now() + 5 * 24 * 60 * 60 * 1000;
+  // 	const id = selectedInvoiceId;
+  // 	console.log(`${API}/download/${id}`);
+  // 	const pdfUrl = `${id}/${expirationTimestamp}`;
+  // 	// Assuming 'navigate' is a function for navigating in your application
+  // 	// You may need to replace it with the appropriate navigation logic
+  // 	navigate(`/pdf/${pdfUrl}`);
+  // 	console.log('Handling Original Invoice');
+  // };
+
+  const handleView = () => {
     if (isMobile) {
       const newWindow = window.open();
       const newIframe = newWindow.document.createElement("iframe");
@@ -50,17 +55,31 @@ const AdminInvoiceAccordion = ({ invoice, code, pdfUrl, preSignedUrl }) => {
       newIframe.style.height = "100%";
       newWindow.document.body.appendChild(newIframe);
     }
-    // Open in a new tab for non-mobile devices
     if (!isMobile) {
       window.open(selectedPdfUrl);
     }
- };
+  };
 
- // Function to copy the original invoice link
- const handleOriginalCopy = async () => {
+  // const handleInvoiceDownload = () => {
+  // 	// window.location = `${API}download/${selectedInvoiceId}`;
+  // };
+
+  const handleOriginalCopy = async () => {
     try {
-      // API request to shorten the URL
-      const Response = await axios.request({
+      const apiKey = encodeURIComponent("+tRfF6lilDDsaSv2SlTB1A==");
+      const csrfToken = encodeURIComponent(
+        "dQoAMh4zBVIWHQNgKjo7bSxzGVQVOwQY0r4DZUr9BoT5bJo_y7k7QmGV"
+      );
+      const workspaceId = 175208;
+      const requestData = {
+        url: preSignedUrl,
+        workspace_id: workspaceId,
+        expiry_datetime: new Date(
+          Date.now() + hours * 60 * 60 * 1000
+        ).toISOString(), // 6 hours from now
+      };
+
+      const options = {
         method: "POST",
         url: `https://app.linklyhq.com/api/v1/link?api_key=${apiKey}`,
         headers: {
@@ -68,82 +87,101 @@ const AdminInvoiceAccordion = ({ invoice, code, pdfUrl, preSignedUrl }) => {
           "Content-Type": "application/json",
           "x-csrf-token": csrfToken,
         },
-        data: {
-          url: preSignedUrl,
-          workspace_id: workspaceId,
-          expiry_datetime: new Date(
-            Date.now() + hours * 60 * 60 * 1000
-          ).toISOString(), // assgined hours from now default is 24 at present
-        },
-      });
+        data: requestData,
+      };
+
+      const Response = await axios.request(options);
       const shortenedUrl = Response.data.full_url;
-      // Copy the shortened URL to clipboard
+      // Copy the shortened URL
       copy(shortenedUrl);
       toast.success("Shortened link copied to clipboard!");
     } catch (error) {
       console.error("Error:", error);
       toast.error("Error generating or copying the shortened link.");
     }
- };
+  };
 
- // Function to copy the invoice code
- const handleCodeCopy = () => {
+  const handleCodeCopy = () => {
     const code = selectedCode;
     const linkToCopy = `${code}`;
     try {
       copy(linkToCopy);
+      // alert('Code copied to clipboard!');
       toast.success("Code copied to clipboard!");
     } catch (error) {
       console.error("Unable to copy to clipboard.", error);
+      // alert('Error copying to clipboard. Please try again.');
       toast.error("Error copying to clipboard. Please try again.");
     }
- };
+  };
 
- // Function to delete the invoice
- const handleDelete = () => {
+  const handleDelete = () => {
+    // console.log('delete');
     const confirmDelete = window.confirm(
       "Are you sure, you want to Delete Invoice?\n\nNote: Once deleted can't be recovered..."
     );
 
     if (confirmDelete) {
-      axios
-        .delete(`${API}invoice/${selectedInvoiceId}`)
-        .then((response) => {
-          toast.success("Invoice deleted successfully");
-          setTimeout(() => {
-            window.location.reload();
-          }, 2000);
-        })
-        .catch((error) => {
-          console.error("Error deleting invoice:", error);
-          toast.error("Error deleting invoice. Please try again.");
-        });
+      if (confirmDelete) {
+        axios
+          .delete(`${API}invoice/${selectedInvoiceId}`)
+          .then((response) => {
+            toast.success("Invoice deleted successfully");
+            setTimeout(() => {
+              // Reload the page after successful deletion
+              window.location.reload();
+            }, 2000);
+          })
+          .catch((error) => {
+            console.error("Error deleting invoice:", error);
+            toast.error("Error deleting invoice. Please try again.");
+          });
+        // console.log('deleted');
+      }
     }
- };
-
- // Function to navigate to the edit invoice page
- const handleEdit = () => {
+  };
+  const handleEdit = () => {
     navigate(`/admineditinv/${selectedInvoiceId}`);
- }
-
- // Function to copy a shortened link using a different method
- const handleCopy2 = async () => {
+  }
+  const handleCopy2 = async () => {
     try {
+      // Encode the selectedInvoiceId to base64
       const encodedUrl = btoa(selectedInvoiceId);
+    
+      // Construct the original URL with the encoded selectedInvoiceId
       const originalUrl = `${API2}${encodedUrl}`;
+    
+      // Generate the short URL
       const shortUrl = await generateShortUrl(originalUrl);
+    
+      // Copy the short URL
       copy(shortUrl);
+    
+      // Show success message
       toast.success("Shortened link copied to clipboard!");
     } catch (error) {
       console.error("Error:", error);
       toast.error("Error copying the shortened link.");
     }
- };
+  };
+  
 
- // Function to generate a short URL
- const generateShortUrl = async (originalUrl) => {
+  const generateShortUrl = async (originalUrl) => {
     try {
-      const response = await axios.request({
+      const apiKey = encodeURIComponent("+tRfF6lilDDsaSv2SlTB1A==");
+      const csrfToken = encodeURIComponent(
+        "dQoAMh4zBVIWHQNgKjo7bSxzGVQVOwQY0r4DZUr9BoT5bJo_y7k7QmGV"
+      );
+      const workspaceId = 175208;
+      const requestData = {
+        url: originalUrl,
+        workspace_id: workspaceId,
+        expiry_datetime: new Date(
+          Date.now() + hours * 60 * 60 * 1000 // 30 minutes from now
+        ).toISOString(),
+      };
+  
+      const options = {
         method: "POST",
         url: `https://app.linklyhq.com/api/v1/link?api_key=${apiKey}`,
         headers: {
@@ -151,32 +189,24 @@ const AdminInvoiceAccordion = ({ invoice, code, pdfUrl, preSignedUrl }) => {
           "Content-Type": "application/json",
           "x-csrf-token": csrfToken,
         },
-        data: {
-          url: originalUrl,
-          workspace_id: workspaceId,
-          expiry_datetime: new Date(
-            Date.now() + hours * 60 * 60 * 1000 // assigned minutes from now default is 24 at present
-          ).toISOString(),
-        },
-      });
+        data: requestData,
+      };
+  
+      const response = await axios.request(options);
       return response.data.full_url;
     } catch (error) {
       console.error("Error generating short URL:", error);
       throw error;
     }
- };
-
- // Function to view the invoice using a different method
- const handleView2 = () => {
+  };
+  const handleView2 = () => {
     const encodedUrl = btoa(selectedInvoiceId);
-    const originalUrl = `${API2}${encodedUrl}`;
+      const originalUrl = `${API2}${encodedUrl}`;
     window.open(originalUrl);
- }
-
- return (
+  }
+  return (
     <div>
       <div>
-        {/* Button to toggle the accordion */}
         <button
           onClick={toggleAccordion}
           className="invoice-management-data-body-table-data-button"
@@ -185,37 +215,33 @@ const AdminInvoiceAccordion = ({ invoice, code, pdfUrl, preSignedUrl }) => {
           {isAccordionOpen && (
             <div className="accordion-popover">
               <div className="modal-btn-div-pdf-inv">
-                {/* Button to view the invoice */}
                 <button
-                 type="button"
-                 className="modal-btn-inv"
-                 onClick={handleView}
+                  type="button"
+                  className="modal-btn-inv"
+                  onClick={handleView}
                 >
-                 View Invoice
+                  View Invoice
                 </button>
-                {/* Button to copy the original invoice link */}
+                {/* <button className='modal-btn-inv' onClick={handleInvoiceDownload}>
+								Download
+							</button> */}
                 <button className="modal-btn-inv" onClick={handleOriginalCopy}>
-                 Copy Link
+                  Copy Link
                 </button>
-                {/* Button to copy the invoice code */}
                 <button className="modal-btn-inv" onClick={handleCodeCopy}>
-                 Copy Code
+                  Copy Code
                 </button>
-                {/* Button to delete the invoice */}
                 <button className="modal-btn-inv" onClick={handleDelete}>
-                 Delete
+                  Delete
                 </button>
-                {/* Button to edit the invoice */}
                 <button className="modal-btn-inv" onClick={handleEdit}>
-                 Edit
+                  Edit
                 </button>
-                {/* Button to copy a shortened link using a different method */}
                 <button className="modal-btn-inv" onClick={handleCopy2}>
-                 Copy Link 2
+                  Copy Link 2
                 </button>
-                {/* Button to view the invoice using a different method */}
                 <button className="modal-btn-inv" onClick={handleView2}>
-                 View Invoice 2
+                  View Invoice 2
                 </button>
               </div>
             </div>
@@ -223,7 +249,7 @@ const AdminInvoiceAccordion = ({ invoice, code, pdfUrl, preSignedUrl }) => {
         </button>
       </div>
     </div>
- );
+  );
 };
 
 export default AdminInvoiceAccordion;
